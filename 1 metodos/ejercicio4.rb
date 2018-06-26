0d8ec6e00dc8465d54277a1f5d08bac0da91274e
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-25 21:13:29 -0400

    Resolver '1 metodos/ejercicio4.rb'

--- 1 metodos/ejercicio4.rb
+++ 1 metodos/ejercicio4.rb
@@ -1,3 +1,9 @@
 # Crear un metodo que muestre en pantalla un saludo, el metodo
 # debe recibir un parametro, si ese parametro es el string "Hola" el
 # metodo debe mostrar en pantalla "Hola Mundo".
+
+def saludar(saludo)
+  puts 'Hola Mundo' if saludo == 'hola'
+end
+
+saludar('hola')
