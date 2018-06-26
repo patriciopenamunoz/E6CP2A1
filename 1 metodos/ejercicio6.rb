10b418b5689bc643ad3351a41f34286c42c1be4f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-25 21:35:23 -0400

    Seguir convenciones según Rubocop.

--- 1 metodos/ejercicio6.rb
+++ 1 metodos/ejercicio6.rb
@@ -1,10 +1,10 @@
 # Un metodo puede llamar a otros metodos, modifica el siguiente codigo
 # para que al llamar a un solo metodo se imprima:
-# *****
-# *****
-# *****
-# *****
-# *****
+# *****
+# *****
+# *****
+# *****
+# *****
 
 def draw_line(size)
   '*' * size
