e8d5d6903e007168571325057da45308e7fb41cf
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-25 21:05:13 -0400

    Resolver '1 metodos/ejercicio3.rb'

--- 1 metodos/ejercicio3.rb
+++ 1 metodos/ejercicio3.rb
@@ -1,5 +1,9 @@
 # Escriba un metodo llamado check5 que devuelva true cuando
 # se le pase un numero mayor a 5 y false en caso contrario.
 
+def check5(num)
+  num > 5
+end
+
 puts check5(5) # Deberia ser false
 puts check5(6) # Deberia ser true
