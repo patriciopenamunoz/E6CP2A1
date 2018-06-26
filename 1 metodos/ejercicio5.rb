f571d268784b371e538d95de6e0be2d51ced9c1f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-25 21:34:31 -0400

    Resolver '1 metodos/ejercicio5.rb'

--- 1 metodos/ejercicio5.rb
+++ 1 metodos/ejercicio5.rb
@@ -1,2 +1,16 @@
 # Crear un metodo que reciba como parametro dos numeros enteros positivos
 # e imprima los numeros pares que existen entre esos dos numeros.
+
+def obtener_pares(num1, num2)
+  inicio = num1 < num2 ? num1 : num2
+  final = num1 > num2 ? num1 : num2
+  inicio.upto(final) do |i|
+    puts i if i.even?
+  end
+end
+
+puts 'Escriba el primer valor:'
+valor1 = gets.chomp.to_i
+puts 'Escriba el segundo valor:'
+valor2 = gets.chomp.to_i
+obtener_pares(valor1, valor2)
