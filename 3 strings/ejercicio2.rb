48ac183420c1febf542cb3de3c79be50d4b58060
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-26 23:15:22 -0400

    Resolver '3 strings/ejercicio2.rb'

--- 3 strings/ejercicio2.rb
+++ 3 strings/ejercicio2.rb
@@ -3,3 +3,19 @@
 # 2. Crear un arreglo nuevo con todos los elementos en minuscula.
 # 3. Crear un metodo que devuelva un arreglo con la cantidad de caracteres
 # que tiene cada nombre.
+
+companeros = [
+  'Tomás', 'Susana', 'Patricio', 'Juan', 'Jorge', 'Daniela', 'Felipe',
+  'Sebastian', 'Vladimir', 'Felipe', 'Gabriela', 'Leudis', 'Marcelo',
+  'Daniela', 'Leonardo', 'Damian', 'Juan', 'Maximiliano', 'Frank'
+]
+# 1
+companeros.each { |companero| puts companero if companero.length > 5 }
+# 2
+en_minuscula = companeros.map(&:downcase)
+puts en_minuscula
+# 3
+def imprimir_largos(listado)
+  listado.each { |item| puts "#{item} = #{item.length}" }
+end
+imprimir_largos(companeros)
