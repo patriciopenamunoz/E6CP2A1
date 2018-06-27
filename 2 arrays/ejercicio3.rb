61c65f027bd72e11a7e8ca01f7d946d432290a39
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2018-06-26 22:19:46 -0400

    Resolver '2 arrays/ejercicio3.rb'

--- 2 arrays/ejercicio3.rb
+++ 2 arrays/ejercicio3.rb
@@ -7,3 +7,28 @@
 # en una unidad y devuelva un arreglo nuevo.
 
 a = [1, 2, 3, 9, 1, 4, 5, 2, 3, 6, 6]
+
+# 1
+def eliminar_pares(arreglo)
+  arreglo.reject(&:even?)
+end
+print "#{eliminar_pares(a)}\n"
+# 2
+def sumar_elementos(arreglo)
+  sumatoria = 0
+  arreglo.each { |elemento| sumatoria += elemento }
+  sumatoria
+end
+print "#{sumar_elementos(a)}\n"
+# 3
+def obtener_promedio(arreglo)
+  sumar_elementos(arreglo) / arreglo.length.to_f
+end
+print "#{obtener_promedio(a)}\n"
+# 4
+def incrementar_elementos(arreglo)
+  nuevo_arreglo = []
+  arreglo.each { |elemento| nuevo_arreglo.push(elemento + 1) }
+  nuevo_arreglo
+end
+print "#{incrementar_elementos(a)}\n"
